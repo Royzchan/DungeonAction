919950bd90241ab06d8ca8ebf1d78feae8e2a51a
林　利樹 <117792737+Royzchan@users.noreply.github.com>
2025-01-16 09:46:45 +0900

    プレイヤーコントローラーに必要な関数を追加

--- DungeonAction/Assets/Scripts/PlayerController.cs
+++ DungeonAction/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float _maxHP = 100f;
+    private float _hp;
+    [SerializeField]
+    private float _maxStamina;
+    private float _stamina;
+    [SerializeField]
+    private float _attackPower = 100f;
+    [SerializeField]
+    private float _defenses = 100f;
+    [SerializeField]
+    private float _speed = 5f;
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
+    }
+
+    //ˆÚ“®
+    private void Move()
+    {
+
+    }
+
+    //UŒ‚
+    private void Attack()
+    {
+
+    }
+
+    //ƒXƒLƒ‹
+    private void Skill()
+    {
+
+    }
+
+    //•KE‹Z
+    private void Special()
+    {
+
     }
 }
