6f8f0bcf4625c3e9f7e72a3a2c4c463b7ba7588c
林　利樹 <117792737+Royzchan@users.noreply.github.com>
2025-01-16 17:00:26 +0900

    プレイヤーの移動を追加

--- DungeonAction/Assets/Scripts/PlayerController.cs
+++ DungeonAction/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody _rb;
+
     [SerializeField]
     private float _maxHP = 100f;
     private float _hp;
@@ -17,20 +20,38 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _speed = 5f;
 
+    [SerializeField]
+    private InputAction _moveXAction;
+
+    private Vector2 _moveDirection = Vector2.zero;
+
+    private void OnEnable()
+    {
+        _moveXAction?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _moveXAction?.Disable();
+    }
+
+
+
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-
+        _moveDirection.x = _moveXAction.ReadValue<float>();
+        Move();
     }
 
     //à⁄ìÆ
     private void Move()
     {
-
+        _rb.velocity = new Vector3(_moveDirection.x * _speed, 0f, _moveDirection.y * _speed);
     }
 
     //çUåÇ
